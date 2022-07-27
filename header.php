f078dccb56344dfa18384c3d09acde97eeed5917
Nate J.M <nate@natejonah.com>
2022-07-27 08:55:39 +0100

    Add link around h1 in header

---
+++ header.php
@@ -0,0 +1,63 @@
+
+<?php
+/**
+ * Theme header
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package natejonah
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+    <header class="header">
+        
+        <div class="container">
+
+            <div class="row">
+                <div class=" logo-col col-12 col-md-4">
+    
+                    <div class="title">
+                        <h1>
+                            <a href="<?php echo get_bloginfo('url'); ?>">Nate</a>
+                        </h1>
+                    </div>
+            
+                </div>
+    
+                <div class="nav-col col-12 col-md-8">
+    
+                    <?php 
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'main-menu',
+                                'menu_id' => 'main-nav'
+                            )
+                        );
+                    ?>
+    
+                </div>
+            </div>
+
+        </div>
+
+    </header>
