81fb1c3911690ee51e9277e82d4b6c0254fb6f0e
Nate J.M <nate@natejonah.com>
2022-07-31 17:19:08 +0100

    Set up theme footer

---
+++ footer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package natejonah
+ */
+
+?>
+
+    <footer class="footer">
+        <div class="container">
+
+            <ul class="social-icons">
+                <li>
+                    <a href="https://twitter.com/natejonah" target="_blank">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/ntjnh" target="_blank">
+                        <i class="fab fa-github" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/nate-manyande/" target="_blank">
+                        <i class="fab fa-linkedin-in" aria-hidden="true"></i>
+                    </a>
+                </li>
+            </ul>
+
+            <p class="d-inline-block small">&copy; <?php echo get_bloginfo('name') . ' ' . date('Y'); ?></p>
+
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
