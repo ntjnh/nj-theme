3d7af5a6106d23fedcef38a656e283832af55988
Nate J.M <nate@natejonah.com>
2022-07-31 17:19:45 +0100

    Create gulp JS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { dest, parallel, series, src, watch} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cssMin = require("gulp-cssmin");
+const terser = require("gulp-terser");
 
 const styles = () => {
     return src("assets/scss/app.scss")
@@ -9,10 +10,17 @@ const styles = () => {
         .pipe(dest("build/css"));
 };
 
+const scripts = () => {
+    return src("assets/js/app.js")
+        .pipe(terser())
+        .pipe(dest("build/js"));
+};
+
 const watchSass = () => watch("./assets/scss/**/*.scss", styles);
 
 module.exports = {
-    default: series(styles),
+    default: series(styles, scripts),
     sass: styles,
+    js: scripts,
     watch: watchSass
 };
