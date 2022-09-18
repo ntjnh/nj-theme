aaa9fc69b514c01080b8f52f46185c1653b1481a
Nate J.M <nate@natejonah.com>
2022-09-19 00:04:16 +0100

    Add columns to about template

--- page-about.php
+++ page-about.php
@@ -14,38 +14,47 @@ get_header();
 
 		<div class="container">
 
-            <div>
-                <div>
-                    <h2>Hi! My name is Nate Manyande and I’m a web developer based in Leeds. I work for a digital agency called DS.Emotion and I mainly do front-end development, building WordPress websites for various clients in the property industry.</h2>
+            <div class="row">
+                <div class="col-12 col-md-6">
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque diam volutpat commodo sed egestas egestas. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Bibendum est ultricies integer quis auctor. Convallis tellus id interdum velit laoreet id donec. Vestibulum lectus mauris ultrices eros in. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut.</p>
+                    <h2 class="mt-0 mb-4">Hi! My name is Nate Manyande and I’m a web developer based in Leeds. I work for a digital agency called DS.Emotion and I mainly do front-end development, building WordPress websites for various clients in the property industry.</h2>
+
+                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque diam volutpat commodo sed egestas egestas. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Bibendum est ultricies integer quis auctor. Convallis tellus id interdum velit laoreet id donec. Vestibulum lectus mauris ultrices eros in. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut.</p>
 
                     <h3>Skills</h3>
-                    <ul>
-                        <li>HTML5</li>
-                        <li>CSS3 / Sass</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>WordPress</li>
-                        <li>PHP</li>
-                        <li>Version control (Git)</li>
-                        <li>Gulp</li>
-                        <li>Npm</li>
-                    </ul>
-                    <ul>
-                        <li>Adobe Photoshop</li>
-                        <li>Adobe XD</li>
-                        <li>Accessibility</li>
-                        <li>SEO</li>
-                        <li>Adobe Photoshop</li>
-                        <li>Adobe XD</li>
-                        <li>Slack</li>
-                        <li>Confluence</li>
-                    </ul>
+                    <div class="row">
+                        <div class="col-6">
+                            <ul>
+                                <li>HTML5</li>
+                                <li>CSS3 / Sass</li>
+                                <li>JavaScript</li>
+                                <li>React</li>
+                                <li>WordPress</li>
+                                <li>PHP</li>
+                                <li>Version control (Git)</li>
+                                <li>Gulp</li>
+                                <li>Npm</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-6">
+                            <ul>
+                                <li>Adobe Photoshop</li>
+                                <li>Adobe XD</li>
+                                <li>Accessibility</li>
+                                <li>SEO</li>
+                                <li>Adobe Photoshop</li>
+                                <li>Adobe XD</li>
+                                <li>Slack</li>
+                                <li>Confluence</li>
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
 
-                <div>
-                    <img src="https://via.placeholder.com/650x810" alt="">
+                <div class="col-12 col-md-6">
+                    <img class="mw-100" src="https://via.placeholder.com/650x810" alt="">
                 </div>
             </div>
 
