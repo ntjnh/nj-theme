e34ce5db4713613a0bd1159e727ebff2d299465c
Nate J.M <nate@natejonah.com>
2022-09-19 00:04:27 +0100

    Begin styling header and nav

--- header.php
+++ header.php
@@ -28,22 +28,26 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
-    <header class="header">
+    <header class="header py-5">
         
         <div class="container">
 
-            <div class="title">
-                <a href="<?php echo get_bloginfo('url'); ?>">Nate</a>
-            </div>
+            <div class="row">
+                <div class="col-12 col-sm-3">
+                    <a class="header__title" href="<?php echo get_bloginfo('url'); ?>"><?php echo get_bloginfo('title'); ?></a>
+                </div>
 
-            <?php 
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'main-menu',
-                        'menu_id' => 'main-nav'
-                    )
-                );
-            ?>
+                <div class="col-12 col-sm-9">
+                    <?php 
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'main-menu',
+                                'menu_id' => 'main-nav'
+                            )
+                        );
+                    ?>
+                </div>
+            </div>
 
         </div>
 
