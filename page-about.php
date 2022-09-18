3a0e8bcac6a2345297b934da1256174a0af97314
Nate J.M <nate@natejonah.com>
2022-09-18 23:20:19 +0100

    Create About page template

---
+++ page-about.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The About page template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nate
+ */
+
+get_header();
+
+?>
+	<main class="py-5">
+
+		<div class="container">
+
+            <div>
+                <div>
+                    <h2>Hi! My name is Nate Manyande and I’m a web developer based in Leeds. I work for a digital agency called DS.Emotion and I mainly do front-end development, building WordPress websites for various clients in the property industry.</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque diam volutpat commodo sed egestas egestas. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Bibendum est ultricies integer quis auctor. Convallis tellus id interdum velit laoreet id donec. Vestibulum lectus mauris ultrices eros in. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut.</p>
+
+                    <h3>Skills</h3>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3 / Sass</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>WordPress</li>
+                        <li>PHP</li>
+                        <li>Version control (Git)</li>
+                        <li>Gulp</li>
+                        <li>Npm</li>
+                    </ul>
+                    <ul>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe XD</li>
+                        <li>Accessibility</li>
+                        <li>SEO</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe XD</li>
+                        <li>Slack</li>
+                        <li>Confluence</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <img src="https://via.placeholder.com/650x810" alt="">
+                </div>
+            </div>
+
+        </div>
+
+	</main>
+
+<?php get_footer();
