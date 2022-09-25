0127527a17c7cee21a759bd5bd75d3cd0073bc89
Nate J.M <nate@natejonah.com>
2022-09-25 21:00:30 +0100

    Hook ACF fields in to About page

--- page-about.php
+++ page-about.php
@@ -9,6 +9,12 @@
 
 get_header();
 
+$introduction = get_field('introduction');
+$content = get_field('content');
+$skills = get_field('skills');
+$photo = get_field('photo');
+$photo_b = get_field('photo_b');
+
 ?>
 	<main class="about">
 
@@ -17,40 +23,54 @@ get_header();
             <div class="row gx-5">
                 <div class="col-12 col-lg-6 order-2 order-lg-1">
 
-                    <h2 class="about__intro mt-0 mb-3 mb-md-4">Hi! My name is Nate Manyande and I’m a web developer based in Leeds. I work for a digital agency called DS.Emotion and I mainly do front-end development, building WordPress websites for various clients in the property industry.</h2>
+                    <h2 class="about__intro mt-0 mb-3 mb-md-4"><?php echo $introduction; ?></h2>
 
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque diam volutpat commodo sed egestas egestas. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Bibendum est ultricies integer quis auctor. Convallis tellus id interdum velit laoreet id donec. Vestibulum lectus mauris ultrices eros in. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut.</p>
+                    <div class="mb-4">
+                        <?php echo $content; ?>
+                    </div>
 
                     <div class="skills">
+
+                        <?php if( have_rows( 'skills' ) ) : ?>
 
                         <h3 class="skills__title">Skills</h3>
 
                         <ul class="skills__list">
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>SCSS</li>
-                            <li>JavaScript</li>
-                            <li>WordPress</li>
-                            <li>Version control (Git)</li>
-                            <li>Gulp</li>
-                            <li>Npm</li>
-                            <li>Accessibility</li>
-                            <li>SEO</li>
-                            <li>Adobe Photoshop</li>
-                            <li>Adobe XD</li>
+                            <?php while( have_rows( 'skills' ) ) : the_row(); ?>
+                                <li><?php echo get_sub_field('skill'); ?></li>
+                            <?php endwhile; ?>
                         </ul>
+
+                        <?php endif; ?>
 
                     </div>
 
                 </div>
 
                 <div class="col-12 col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0">
-                    <picture>
-                        <source media="(max-width: 767px)" srcset="https://via.placeholder.com/990x1234" />
-                        <source media="(max-width: 991px)" srcset="https://via.placeholder.com/990x495" />
-                        <source media="(min-width: 992px)" srcset="https://via.placeholder.com/990x1234" />
-                        <img class="mw-100" src="https://via.placeholder.com/990x1234" alt="Photo of Nate" />
-                    </picture>
+                    <div class="mb-lg-4">
+                        <picture>
+                            <source media="(max-width: 575px)" srcset="<?php echo $photo['sizes']['photo-xlarge']; ?>" />
+                            <source media="(max-width: 767px)" srcset="<?php echo $photo['sizes']['photo-small']; ?>" />
+                            <source media="(max-width: 991px)" srcset="<?php echo $photo['sizes']['photo-medium']; ?>" />
+                            <source media="(max-width: 1199px)" srcset="<?php echo $photo['sizes']['photo-large']; ?>" />
+                            <source media="(max-width: 1499px)" srcset="<?php echo $photo['sizes']['photo-xlarge']; ?>" />
+                            <source media="(min-width: 1500px)" srcset="<?php echo $photo['sizes']['photo-xxlarge']; ?>" />
+                            <img class="mw-100" src="<?php echo $photo['sizes']['photo-medium']; ?>" alt="Photo of Nate" />
+                        </picture>
+                    </div>
+
+                    <div class="d-none d-lg-block">
+                        <picture>
+                            <source media="(max-width: 575px)" srcset="<?php echo $photo_b['sizes']['photo-xlarge']; ?>" />
+                            <source media="(max-width: 767px)" srcset="<?php echo $photo_b['sizes']['photo-small']; ?>" />
+                            <source media="(max-width: 991px)" srcset="<?php echo $photo_b['sizes']['photo-medium']; ?>" />
+                            <source media="(max-width: 1199px)" srcset="<?php echo $photo_b['sizes']['photo-large']; ?>" />
+                            <source media="(max-width: 1499px)" srcset="<?php echo $photo_b['sizes']['photo-xlarge']; ?>" />
+                            <source media="(min-width: 1500px)" srcset="<?php echo $photo_b['sizes']['photo-xxlarge']; ?>" />
+                            <img class="mw-100" src="<?php echo $photo_b['sizes']['photo-medium']; ?>" alt="Photo of Nate" />
+                        </picture>
+                    </div>
                 </div>
             </div>
 
