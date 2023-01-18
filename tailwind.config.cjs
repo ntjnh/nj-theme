2bea3c0ffc5bdd8a31aec7a6b1e6e411cd6144fa
Nate J.M <nate@natejonah.com>
2023-01-18 23:15:59 +0000

    Add custom values to TW theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./assets/**/*.js", "./*.php"],
+    content: [
+        './assets/**/*.js',
+        './*.php'
+    ],
     theme: {
+        screens: {
+            sm: '576px',
+            '3xl': '1800px'
+        },
+        fontFamily: {
+            serif: [
+                'bely',
+                'Merriweather',
+                ...defaultTheme.fontFamily.serif
+            ]
+        },
+        colors: {
+            black: '#222',
+            blue: '#0052bd',
+            yellow: '#fde66b'
+        },
         extend: {},
     },
     plugins: [],
