c95db546c7192c523be479dd2ee19ecc07fd6a5d
Nate J.M <nate@natejonah.com>
2023-01-21 14:27:20 +0000

    Replace BS classes with TW classes in header.php

--- header.php
+++ header.php
@@ -28,23 +28,25 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
-    <header class="header py-4 py-md-5">
+    <header class="header py-4 md:py-5">
         
-        <div class="container">
+        <div class="container mx-auto">
 
-            <div class="row">
-                <div class="col-12 col-sm-3 mb-3 mb-sm-0">
-                    <h1 class="header__title block font-normal leading-none m-0 md:text-left text-6xl text-black text-center tracking-tighter uppercase">
+            <div class="w-full flex flex-wrap">
+                <div class="w-full sm:w-4/12 md:w-3/12 mb-3 sm:mb-0">
+                    <h1 class="block font-normal leading-none m-0 text-6xl text-black text-center sm:text-left tracking-tighter uppercase">
                         <a class="no-underline" href="<?php echo get_bloginfo('url'); ?>"><?php echo get_bloginfo('title'); ?></a>
                     </h1>
                 </div>
 
-                <div class="col-12 col-sm-9 header__nav-col">
+                <div class="w-full sm:w-8/12 md:w-9/12 flex flex-nowrap items-center justify-center sm:justify-end">
                     <?php 
                         wp_nav_menu(
                             array(
                                 'theme_location' => 'main-menu',
-                                'menu_id' => 'main-nav'
+                                'menu_id' => 'main-nav',
+                                'container' => 'nav',
+                                'container_class' => 'header-nav'
                             )
                         );
                     ?>
