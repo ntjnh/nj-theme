1a7499306378ed39d91e5c38debc3fcb29d55841
Nate J.M <nate@natejonah.com>
2023-02-12 20:26:47 +0000

    Remove enqueue of old CSS file

--- functions.php
+++ functions.php
@@ -38,7 +38,6 @@ add_action( 'after_setup_theme', 'nate_setup' );
 function nate_scripts() {
     wp_enqueue_style( 'adobe-fonts', 'https://use.typekit.net/xop5aup.css', array(), false, 'all' );	
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap', array(), false, 'all' );	
-    wp_enqueue_style( 'nate-style', get_template_directory_uri() . '/build/css/app.css', array(), NATE_VERSION, 'all' );	
     wp_enqueue_style( 'tailwind', get_template_directory_uri() . '/build/css/tw.css', array(), '3.2.4', 'all' );
 
 	wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/6e2ccb04f1.js?ver=5.15.3', array(), '5.15.3', false );
