4e9adfbf1bef984a759448ada9441b0abf591c30
Nate J.M <nate@natejonah.com>
2023-02-12 20:26:27 +0000

    Adjust 404 template structure and classes

--- 404.php
+++ 404.php
@@ -15,24 +15,27 @@
 get_header();
 
 ?>
-	<main class="font-serif text-lg pb-5">
+	<main class="font-serif text-lg pb-12">
         
         <div class="container sm:max-w-box-sm md:max-w-box-md lg:max-w-box-lg xl:max-w-box-xl 2xl:max-w-screen-xl px-6 md:px-3">
-            <header class="entry-header mb-6">
-                <h2 class="font-bold text-4xl"><?php esc_html_e( 'Page Not Found', 'goal' ); ?></h2>
-            </header>
 
-            <div>
-				<p class="mb-4">Sorry but the page you're looking for can't be found. It may have been removed, renamed, or is temporarily unavailable.</p>
+			<article class="max-w-3xl mx-auto w-full">
 
-				<p class="mb-4">You could try:</p>
+				<h1 class="font-bold mb-8 mt-0 text-3xl md:text-4xl xl:text-4xxl"><?php esc_html_e( 'Page Not Found', 'nate' ); ?></h1>
+	
+				<div class="text-lg">
+					<p>Sorry but the page you're looking for can't be found. It may have been removed, renamed, or is temporarily unavailable.</p>
+	
+					<p>You could try:</p>
+	
+					<ul class="list-disc pl-8">
+						<li>Double-checking your spelling</li>
+						<li>Going back to the <a class="underline" href="<?php echo get_bloginfo('url'); ?>">homepage</a></li>
+						<li><a class="underline" href="javascript:history.back()">Going back</a> to the page you came from</li>
+					</ul>
+				</div>
 
-				<ul class="list-disc pl-8">
-					<li>Double-checking your spelling</li>
-					<li>Going back to the <a class="underline" href="<?php echo get_bloginfo('url'); ?>">homepage</a></li>
-					<li><a class="underline" href="javascript:history.back()">Going back</a> to the page you came from</li>
-				</ul>
-			</div>
+			</article>
 
         </div>
 
