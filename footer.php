5c10dd57b5a0bc72f107e8df90246acb62eaa15f
Nate J.M <nate@natejonah.com>
2023-07-06 12:01:43 -0700

    Add FA 6 classes to social icons

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,46 +12,47 @@
 
 ?>
 
-    <footer class="footer pb-6 pt-12">
+<footer class="footer pb-6 pt-12">
 
-        <div class="container sm:max-w-box-sm md:max-w-box-md lg:max-w-[82.5%] 2xl:max-w-screen-xl px-6 md:px-3 mx-auto">
+    <div class="container sm:max-w-box-sm md:max-w-box-md lg:max-w-[82.5%] 2xl:max-w-screen-xl px-6 md:px-3 mx-auto">
 
-            <div class="flex flex-wrap">
-                <div class="w-full sm:w-6/12">
-                    <ul class="leading-none m-0 p-0">
-                        <li class="leading-none inline-block text-2xl list-none mr-2">
-                            <a class="hover:text-blue" href="https://github.com/ntjnh" target="_blank">
-                                <i class="fab fa-github" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li class="leading-none inline-block text-2xl list-none">
-                            <a class="hover:text-blue" href="https://www.linkedin.com/in/nate-manyande/" target="_blank">
-                                <i class="fab fa-linkedin-in" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="flex flex-nowrap items-center justify-end w-full sm:w-6/12">
-                    <?php 
-                        wp_nav_menu(
-                            array(
-                                'theme_location' => 'footer-menu',
-                                'menu_id' => 'footer-nav',
-                                'container' => 'div',
-                                'container_class' => 'footer-nav'
-                            )
-                        );
-                    ?>
-
-                    <p class="border border-r-0 border-y-0 border-blue-980 border-solid ml-3 my-0 pl-3 text-right text-sm">&copy; <?php echo get_bloginfo('name') . ' ' . date('Y'); ?></p>
-                </div>
+        <div class="flex flex-wrap">
+            <div class="w-full sm:w-6/12">
+                <ul class="leading-none m-0 p-0">
+                    <li class="leading-none inline-block text-2xl list-none mr-2">
+                        <a class="hover:text-blue" href="https://github.com/ntjnh" target="_blank">
+                            <i class="fa-brands fa-github"></i>
+                        </a>
+                    </li>
+                    <li class="leading-none inline-block text-2xl list-none">
+                        <a class="hover:text-blue" href="https://www.linkedin.com/in/nate-manyande/" target="_blank">
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
 
+            <div class="flex flex-nowrap items-center justify-end w-full sm:w-6/12">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'footer-menu',
+                        'menu_id' => 'footer-nav',
+                        'container' => 'div',
+                        'container_class' => 'footer-nav'
+                    )
+                );
+                ?>
+
+                <p class="border border-r-0 border-y-0 border-blue-980 border-solid ml-3 my-0 pl-3 text-right text-sm">&copy; <?php echo get_bloginfo('name') . ' ' . date('Y'); ?></p>
+            </div>
         </div>
 
-    </footer>
+    </div>
 
-    <?php wp_footer(); ?>
+</footer>
+
+<?php wp_footer(); ?>
 </body>
+
 </html>
