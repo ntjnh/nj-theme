77ef78a86f458db7f9c420860677c8881796ee15
Nate J.M <nate@natejonah.com>
2023-07-06 09:51:05 -0700

    Bump version numbers

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 
 if ( ! defined( 'NATE_VERSION' ) ) {
-	define( 'NATE_VERSION', '2.0.2' );
+    define( 'NATE_VERSION', '3.0.0' );
 }
 
 function nate_setup() {
@@ -16,20 +16,20 @@ function nate_setup() {
 
     add_theme_support( 'post-thumbnails' );
 
-	// Additional image sizes
-	add_image_size( 'photo-small', 492, 277, true );
-	add_image_size( 'photo-medium', 696, 392, true );
-	add_image_size( 'photo-large', 389, 219, true );
-	add_image_size( 'photo-xlarge', 558, 314, true );
-	add_image_size( 'photo-xxlarge', 604, 340, true );
+    // Additional image sizes
+    add_image_size( 'photo-small', 492, 277, true );
+    add_image_size( 'photo-medium', 696, 392, true );
+    add_image_size( 'photo-large', 389, 219, true );
+    add_image_size( 'photo-xlarge', 558, 314, true );
+    add_image_size( 'photo-xxlarge', 604, 340, true );
 
     // Menus
-	register_nav_menus(
-		array(
-			'main-menu' => esc_html__( 'Primary', 'nate' ),
+    register_nav_menus(
+        array(
+            'main-menu' => esc_html__( 'Primary', 'nate' ),
             'footer-menu' => esc_html__( 'Footer', 'nate' )
-		)
-	);
+        )
+    );
 }
 
 add_action( 'after_setup_theme', 'nate_setup' );
@@ -40,10 +40,10 @@ function nate_scripts() {
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap', array(), false, 'all' );	
     wp_enqueue_style( 'tailwind', get_template_directory_uri() . '/build/css/tw.css', array(), '3.2.4', 'all' );
 
-	wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/6e2ccb04f1.js?ver=5.15.3', array(), '5.15.3', false );
-	
-	wp_enqueue_script( 'jquery-masonry', array( 'jquery' ), NATE_VERSION, true );
-	wp_enqueue_script( 'nate-script', get_template_directory_uri() . '/build/js/app.js', array( 'jquery' ), NATE_VERSION, true );
+    wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/6e2ccb04f1.js?ver=5.15.3', array(), '5.15.3', false );
+    
+    wp_enqueue_script( 'jquery-masonry', array( 'jquery' ), NATE_VERSION, true );
+    wp_enqueue_script( 'nate-script', get_template_directory_uri() . '/build/js/app.js', array( 'jquery' ), NATE_VERSION, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'nate_scripts' );
@@ -61,16 +61,16 @@ function project_cpt() {
             'labels' => array(
                 'name' => __( 'Projects' ),
                 'singular_name' => __( 'Project' ),
-				'name'                => _x( 'Projects', 'Post Type General Name', 'nate' ),
-				'singular_name'       => _x( 'Project', 'Post Type Singular Name', 'nate' ),
-				'menu_name'           => __( 'Projects', 'nate' ),
-				'parent_item_colon'   => __( 'Parent Project', 'nate' ),
-				'all_items'           => __( 'All Projects', 'nate' ),
-				'view_item'           => __( 'View Project', 'nate' ),
-				'add_new_item'        => __( 'Add New Project', 'nate' ),
-				'edit_item'           => __( 'Edit Project', 'nate' ),
-				'update_item'         => __( 'Update Project', 'nate' ),
-				'search_items'        => __( 'Search Project', 'nate' ),
+                'name'                => _x( 'Projects', 'Post Type General Name', 'nate' ),
+                'singular_name'       => _x( 'Project', 'Post Type Singular Name', 'nate' ),
+                'menu_name'           => __( 'Projects', 'nate' ),
+                'parent_item_colon'   => __( 'Parent Project', 'nate' ),
+                'all_items'           => __( 'All Projects', 'nate' ),
+                'view_item'           => __( 'View Project', 'nate' ),
+                'add_new_item'        => __( 'Add New Project', 'nate' ),
+                'edit_item'           => __( 'Edit Project', 'nate' ),
+                'update_item'         => __( 'Update Project', 'nate' ),
+                'search_items'        => __( 'Search Project', 'nate' ),
             ),
             'public' => true,
             'has_archive' => false,
@@ -79,16 +79,16 @@ function project_cpt() {
                 'with_front' => false
             ),
             'show_in_rest' => false,
-			'menu_icon' => 'dashicons-portfolio',
-			'hierarchical' => false,
-			'supports' => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields' ),
-			'show_ui'             => true,
-			'show_in_menu'        => true,
-			'show_in_nav_menus'   => false,
-			'show_in_admin_bar'   => false,
-			'exclude_from_search' => false,
-			'publicly_queryable'  => true,
-			'capability_type'     => 'post'  
+            'menu_icon' => 'dashicons-portfolio',
+            'hierarchical' => false,
+            'supports' => array( 'title', 'editor', 'thumbnail', 'revisions', 'custom-fields' ),
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => false,
+            'show_in_admin_bar'   => false,
+            'exclude_from_search' => false,
+            'publicly_queryable'  => true,
+            'capability_type'     => 'post'  
         )
     );
 }
