a27b7f6b7cf1e094dde1b8e68fe2f08c463f678e
Nate J.M <nate@natejonah.com>
2023-10-14 23:43:32 +0100

    Add missing HTML code

--- index.php
+++ index.php
@@ -12,9 +12,24 @@
  * @package nate
  */
 
-get_header();
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/wp-content/themes/nj-theme/style.css">
+
+    <title>NATE</title>
+</head>
+<body>
+    <header>
+        <h1>Nate</h1>
+    </header>
+
 	<main class="py-12">
 
 		<div class="container sm:max-w-box-sm md:max-w-box-md lg:max-w-[82.5%] 2xl:max-w-screen-xl px-6 md:px-3">
@@ -48,4 +63,6 @@ get_header();
 
 	</main>
 
-<?php get_footer();
+</body>
+
+</html>
