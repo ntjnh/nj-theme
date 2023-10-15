a97057c1b0e2710766001f944a3ef3bbb9c4ca8d
Nate J.M <nate@natejonah.com>
2023-10-15 09:29:50 +0100

    Change font awesome link

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ function nate_scripts() {
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap', array(), false, 'all' );	
     wp_enqueue_style( 'tailwind', get_template_directory_uri() . '/build/css/tw.css', array(), '3.2.4', 'all' );
 
-    wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/6e2ccb04f1.js', array(), '6.4.0', false );
+    wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/565d8eb534.js', array(), '6.4.2', false );
     
     wp_enqueue_script( 'jquery-masonry', array( 'jquery' ), NATE_VERSION, true );
     wp_enqueue_script( 'nate-script', get_template_directory_uri() . '/build/js/app.js', array( 'jquery' ), NATE_VERSION, true );
